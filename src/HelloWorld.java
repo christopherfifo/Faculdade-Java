2de6c0233f4c98d46088afac5804a1e2f0cc023f
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2025-02-24 22:38:55 -0300

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	
+	public String msg; // atributo da classe
+	
+	 public HelloWorld() {
+		 this.msg = "Hello World!";
+		 
+	}
+	 
+	public void show() {
+		
+		System.out.println(this.msg);
+	}
+}
---
+++ src/incial.java
@@ -0,0 +1,12 @@
+public class incial {
+
+	public static void main(String[] args) {
+		
+		HelloWorld hworld = new HelloWorld(); // a copia ja começa com a função construtora
+		
+		hworld.show();
+		
+	}
+	//o que tiver como static se instancia sozinho, se uma classe não estatica, mas tem uma função estatica a função se incia
+	// nunca usamos uma classe, apenas uma classe estatica
+}
